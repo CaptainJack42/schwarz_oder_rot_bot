5f38df0e50f46cd5fceb944cbf7a42491a91b40d
Janek <janek.graff@gmx.de>
2022-01-22 15:37:38 +0100

    add cog structure for bot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 import logging
 
@@ -7,9 +8,10 @@ load_dotenv()
 API_TOKEN = os.environ.get('API_TOKEN')
 
 
-class SoRClient(discord.Client):
-    def __init__(self) -> None:
+class SoRClient(commands.Cog):
+    def __init__(self, bot: commands.Bot) -> None:
         super().__init__()
+        self.bot = bot
         log_format = (
             '[%(asctime)s] %(levelname)-8s %(name)-12s %(message)s'
         )
@@ -25,12 +27,31 @@ class SoRClient(discord.Client):
         self.logger.addHandler(file_handler)
 
     async def on_ready(self):
+        """doesn't work somehow
+        but should be called on connect
+        """
         guilds = discord.utils.get(client.guilds)
         self.logger.info(
             f'{self.user.display_name} connected'
         )
 
+    @commands.command(name='sor', aliases=['schwarz', 'rot', 'schwarzoderrot', 'schwarz oder rot'])
+    async def create_game(self, ctx):
+        message = await ctx.send(f'{ctx.author.mention} will Schwarz oder Rot spielen, @everyone join in by clicking :heavy_plus_sign:!')
+        await message.add_reaction(':heavy_plus_sign:')  # FIXME
+
+    @commands.command(name='start')
+    async def start_game(self, ctx):
+        pass
+
+    @commands.command(name='hey')
+    async def hey(self, ctx):
+        guild = ctx.guild
+        channel = ctx.channel
+        await ctx.send(f'Hello {ctx.author} and welcome to {channel} in {guild}')
+
 
 if __name__ == '__main__':
-    client = SoRClient()
-    client.run(API_TOKEN)
+    bot = commands.Bot('!')
+    bot.add_cog(SoRClient(bot))
+    bot.run(API_TOKEN)
