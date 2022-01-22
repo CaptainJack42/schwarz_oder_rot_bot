da336ff9055237f98368f29f369d25c4533e6d1c
Janek <janek.graff@gmx.de>
2022-01-22 13:56:43 +0100

    minimal working bot, able to connect to a server

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 import os
+import discord
+from dotenv import load_dotenv
 
-api_key = os.environ.get('DISCORD_API_KEY')
+load_dotenv()
+API_TOKEN = os.environ.get('API_TOKEN')
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    for guild in client.guilds:
+        print(
+            f'{client.user} is connected to: \n',
+            f'{guild.name} (id: {guild.id})'
+        )
+
+if __name__ == '__main__':
+    print(API_TOKEN)
+    client.run(API_TOKEN)
