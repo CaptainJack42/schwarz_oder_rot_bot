9808831bb7f4bbe15f5589f2f3a884620a3cde8e
Janek <janek.graff@gmx.de>
2022-01-22 14:42:57 +0100

    implement logging functionality

--- main.py
+++ main.py
@@ -1,21 +1,36 @@
 import os
 import discord
 from dotenv import load_dotenv
+import logging
 
 load_dotenv()
 API_TOKEN = os.environ.get('API_TOKEN')
 
-client = discord.Client()
 
+class SoRClient(discord.Client):
+    def __init__(self) -> None:
+        super().__init__()
+        log_format = (
+            '[%(asctime)s] %(levelname)-8s %(name)-12s %(message)s'
+        )
+        file_handler = logging.FileHandler('SoR-Log.log')
+        file_handler.setFormatter(logging.Formatter(log_format))
+        file_handler.setLevel(logging.DEBUG)
+        logging.basicConfig(
+            level=logging.DEBUG,
+            format=log_format,
+            filename=('debug.log')
+        )
+        self.logger = logging.getLogger('SoR-Logger')
+        self.logger.addHandler(file_handler)
 
-@client.event
-async def on_ready():
-    for guild in client.guilds:
-        print(
-            f'{client.user} is connected to: \n',
-            f'{guild.name} (id: {guild.id})'
+    async def on_ready(self):
+        guilds = discord.utils.get(client.guilds)
+        self.logger.info(
+            f'{self.user.display_name} connected'
         )
 
+
 if __name__ == '__main__':
-    print(API_TOKEN)
+    client = SoRClient()
     client.run(API_TOKEN)
