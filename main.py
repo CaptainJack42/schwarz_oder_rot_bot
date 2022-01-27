a7f92621b8ee5a05629b58539768ebfa05c5d60e
Janek <janek.graff@gmx.de>
2022-01-27 20:27:30 +0100

    refactor structure to be able to handle reaction events and parse commands properly

--- main.py
+++ main.py
@@ -8,10 +8,37 @@ load_dotenv()
 API_TOKEN = os.environ.get('API_TOKEN')
 
 
-class SoRClient(commands.Cog):
-    def __init__(self, bot: commands.Bot) -> None:
-        super().__init__()
-        self.bot = bot
+class MainClient(commands.Bot):
+    def __init__(self, command_prefix, help_command=commands.bot._default, description=None, **options):
+        super().__init__(command_prefix, help_command, description, **options)
+        self.__setup_logger()
+        self.__register_commands()
+        self.relevant_msg: discord.Message = None
+        self.players: list[discord.User]
+        
+    def __register_commands(self):
+        self.start_sor_game = self.command(name='sor', aliases=['schwarz', 'rot', 'schwarzoderrot', 'schwarz oder rot'], pass_context=True)(self.create_sor_game)
+
+    async def on_ready(self):
+        guilds = discord.utils.get(self.guilds)
+        self.logger.info(
+            f'{self.user.display_name} connected in [{guilds}]'
+        )
+
+    async def create_sor_game(self, ctx):
+        message = await ctx.send(f'{ctx.author.mention} will Schwarz oder Rot spielen, @everyone join in by clicking :beers:!')
+        await message.add_reaction('🍻')
+        self.relevant_msg = message
+
+    async def on_raw_reaction_add(self, payload: discord.RawReactionActionEvent):
+        if payload.message_id != self.relevant_msg.id:
+            return
+        if payload.user_id == self.user.id:
+            return
+        print('reacted')
+        
+            
+    def __setup_logger(self):
         log_format = (
             '[%(asctime)s] %(levelname)-8s %(name)-12s %(message)s'
         )
@@ -26,32 +53,6 @@ class SoRClient(commands.Cog):
         self.logger = logging.getLogger('SoR-Logger')
         self.logger.addHandler(file_handler)
 
-    async def on_ready(self):
-        """doesn't work somehow
-        but should be called on connect
-        """
-        guilds = discord.utils.get(client.guilds)
-        self.logger.info(
-            f'{self.user.display_name} connected'
-        )
-
-    @commands.command(name='sor', aliases=['schwarz', 'rot', 'schwarzoderrot', 'schwarz oder rot'])
-    async def create_game(self, ctx):
-        message = await ctx.send(f'{ctx.author.mention} will Schwarz oder Rot spielen, @everyone join in by clicking :heavy_plus_sign:!')
-        await message.add_reaction(':heavy_plus_sign:')  # FIXME
-
-    @commands.command(name='start')
-    async def start_game(self, ctx):
-        pass
-
-    @commands.command(name='hey')
-    async def hey(self, ctx):
-        guild = ctx.guild
-        channel = ctx.channel
-        await ctx.send(f'Hello {ctx.author} and welcome to {channel} in {guild}')
-
-
 if __name__ == '__main__':
-    bot = commands.Bot('!')
-    bot.add_cog(SoRClient(bot))
+    bot = MainClient(command_prefix='!', description='!SchwarzOderRot')
     bot.run(API_TOKEN)
